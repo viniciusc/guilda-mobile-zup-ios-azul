07d2ccc11b2684560179a723e099bec8c544a28a
Tatiane Silva <tatianefx@gmail.com>
2024-06-13 11:56:25 -0300

    First commit

---
+++ FilmFinder/FilmFinder/FilmFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmFinderApp.swift
+//  FilmFinder
+//
+//  Created by Tatiane Silva on 13/06/24.
+//
+
+import SwiftUI
+
+@main
+struct FilmFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
